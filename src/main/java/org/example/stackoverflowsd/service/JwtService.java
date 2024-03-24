759552056c35a35a2e4d06febda67282447e71ca
Andrei Cadar <cadarandrei2370@yahoo.com>
2024-03-24 22:56:03 +0200

    Documentation

--- src/main/java/org/example/stackoverflowsd/service/JwtService.java
+++ src/main/java/org/example/stackoverflowsd/service/JwtService.java
@@ -18,7 +18,7 @@ import java.util.function.Function;
 @Component
 public class JwtService {
 
-    public static final String SECRET = "5367566B59703373367639792F423F4528482B4D6251655468576D5A71347437";
+    public static final String SECRET = "CELMAIMARESUPERSECREET1029382190jnfkddskvmlksdaksdkskd";
     public String generateToken(String userName) {
         Map<String, Object> claims = new HashMap<>();
         return createToken(claims, userName);
